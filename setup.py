bc545caf97149ae5dff33731ea5f681c35a650e8
Cook Green <cookgreen@users.noreply.github.com>
2018-06-16 15:34:56 +0800

    Setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'PyTinyPic',
+    version = '0.0.1',
+    keywords = ('TinyPic'),
+    description = 'Unoffical TinyPic Library written with Python',
+    license = 'GNU GPLv3',
+
+    author = 'cookgreen',
+    url = 'https://github.com/cookgreen/PyTinyPic',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)
